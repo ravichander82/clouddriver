fbf0f9767832a09281571c5b123bf6f40733cb4d
ravichander82 <ravichander.etikyala@opsmx.io>
2022-02-17 12:33:42 +0530

    feat(gce):to support kork-credentials abstractions

--- clouddriver-google/src/main/groovy/com/netflix/spinnaker/clouddriver/google/provider/view/GoogleSecurityGroupProvider.groovy
+++ clouddriver-google/src/main/groovy/com/netflix/spinnaker/clouddriver/google/provider/view/GoogleSecurityGroupProvider.groovy
@@ -31,6 +31,7 @@ import com.netflix.spinnaker.clouddriver.model.SecurityGroupProvider
 import com.netflix.spinnaker.clouddriver.model.securitygroups.IpRangeRule
 import com.netflix.spinnaker.clouddriver.model.securitygroups.Rule
 import com.netflix.spinnaker.clouddriver.security.AccountCredentialsProvider
+import com.netflix.spinnaker.credentials.CredentialsRepository
 import groovy.util.logging.Slf4j
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
@@ -41,15 +42,15 @@ import static com.netflix.spinnaker.clouddriver.google.cache.Keys.Namespace.SECU
 @Component
 class GoogleSecurityGroupProvider implements SecurityGroupProvider<GoogleSecurityGroup> {
 
-  private final AccountCredentialsProvider accountCredentialsProvider
+  private final CredentialsRepository credentialsRepository
   private final Cache cacheView
   private final ObjectMapper objectMapper
 
   final String cloudProvider = GoogleCloudProvider.ID
 
   @Autowired
-  GoogleSecurityGroupProvider(AccountCredentialsProvider accountCredentialsProvider, Cache cacheView, ObjectMapper objectMapper) {
-    this.accountCredentialsProvider = accountCredentialsProvider
+  GoogleSecurityGroupProvider(CredentialsRepository credentialsRepository, Cache cacheView, ObjectMapper objectMapper) {
+    this.credentialsRepository = credentialsRepository
     this.cacheView = cacheView
     this.objectMapper = objectMapper
   }
@@ -232,7 +233,7 @@ class GoogleSecurityGroupProvider implements SecurityGroupProvider<GoogleSecurit
   }
 
   private String deriveResourceId(String account, String resourceLink) {
-    def accountCredentials = accountCredentialsProvider.getCredentials(account)
+    def accountCredentials = credentialsRepository.getOne(account)
 
     if (!(accountCredentials instanceof GoogleNamedAccountCredentials)) {
       throw new IllegalArgumentException("Invalid credentials: $account")
